a490ce2b7aa78211841959d6eba097cd44edc87e
Mia-Yang <siyuyang1995@gmail.com>
2021-01-14 02:02:55 +0800

    feat: could calculate total cost

--- src/component/OrderSummary/OrderSummary.js
+++ src/component/OrderSummary/OrderSummary.js
@@ -3,20 +3,51 @@ import './OrderSummary.scss';
 import { connect } from 'react-redux';
 
 class OrderSummary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shippingFee: 5,
+    };
+  }
+
+  getTotalCost = (productList) => {
+    let totalCost = 0;
+    for (const product of productList) {
+      let { price, quantity } = product;
+      let productTotal = price * quantity;
+      totalCost += productTotal;
+    }
+    return totalCost;
+  };
+
+  handleChangeShipping = (e) => {
+    this.setState({
+      shippingFee: parseInt(e.target.value),
+    });
+  };
+
   render() {
     const { productList } = this.props;
+    let totalCost = this.getTotalCost(productList);
     return (
       <div className="summary">
         <h1>Order Summary</h1>
         <hr />
         <div>
           <span>ITEMS {productList.length}</span>
-          <span>$</span>
+          <span>$ {totalCost.toFixed(2)}</span>
         </div>
         <h3>SHIPPING</h3>
-        <select name="shipping" id="shipping">
-          <option value="standard">standard shipping</option>
-          <option value="express">express shipping</option>
+        <select
+          name="shipping"
+          id="shipping"
+          onChange={this.handleChangeShipping}
+          value={this.state.value}
+        >
+          <option value="5" selected>
+            standard shipping -$5
+          </option>
+          <option value="10">express shipping -$10</option>
         </select>
         <h3>PROMO CODE</h3>
         <input type="text" placeholder="Enter Your Code" /> <br />
@@ -24,7 +55,7 @@ class OrderSummary extends React.Component {
         <hr />
         <div>
           <span>TOTAL COST</span>
-          <span>$</span>
+          <span>$ {(totalCost + this.state.shippingFee).toFixed(2)}</span>
         </div>
         <button className="checkout">CHECK OUT</button>
       </div>
