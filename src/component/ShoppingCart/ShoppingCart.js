493d12deb65b0cd7111ebb7f6986fcfc31645356
Mia-Yang <siyuyang1995@gmail.com>
2021-01-17 18:50:13 +0800

    feat: modify styles

--- src/component/ShoppingCart/ShoppingCart.js
+++ src/component/ShoppingCart/ShoppingCart.js
@@ -24,45 +24,47 @@ class ShoppingCart extends React.Component {
         </div>
 
         <div className="split"></div>
-        <table>
-          <thead>
-            <tr>
-              <th>product details</th>
-              <th> </th>
-              <th>quantity</th>
-              <th>price</th>
-              <th>total</th>
-            </tr>
-          </thead>
-          <tbody>
-            {productList.map((product) => (
-              <tr className="item" key={product.id}>
-                <CartItem
-                  name={product.name}
-                  label={product.label}
-                  quantity={product.quantity}
-                  imgUrl={product.imgUrl}
-                  price={product.price}
-                  onRemove={() => this.props.removeItemInDB(product.id)}
-                  onIncrease={() =>
-                    this.props.changeQuantityInDB(
-                      product.id,
-                      product.quantity,
-                      1
-                    )
-                  }
-                  onDecrease={() =>
-                    this.props.changeQuantityInDB(
-                      product.id,
-                      product.quantity,
-                      -1
-                    )
-                  }
-                />
+        <div className="cart">
+          <table>
+            <thead>
+              <tr>
+                <th>product details</th>
+                <th> </th>
+                <th>quantity</th>
+                <th>price</th>
+                <th>total</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {productList.map((product) => (
+                <tr className="item" key={product.id}>
+                  <CartItem
+                    name={product.name}
+                    label={product.label}
+                    quantity={product.quantity}
+                    imgUrl={product.imgUrl}
+                    price={product.price}
+                    onRemove={() => this.props.removeItemInDB(product.id)}
+                    onIncrease={() =>
+                      this.props.changeQuantityInDB(
+                        product.id,
+                        product.quantity,
+                        1
+                      )
+                    }
+                    onDecrease={() =>
+                      this.props.changeQuantityInDB(
+                        product.id,
+                        product.quantity,
+                        -1
+                      )
+                    }
+                  />
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
 
         <a href="##" className="continue">
           ← Continue Shopping
