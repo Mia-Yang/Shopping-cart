ed7eb39a28f35e3f63eb6bccc4dbda3aa2991bd6
Mia-Yang <siyuyang1995@gmail.com>
2021-01-18 23:50:02 +0800

    test cartitem

---
+++ src/component/CartItem/CartItem.test.js
@@ -0,0 +1,53 @@
+import CartItem from './CartItem';
+import {
+  fireEvent,
+  getAllByRole,
+  getByRole,
+  render,
+  screen,
+} from '@testing-library/react';
+
+const product = {
+  id: 1,
+  name: 'Fifa',
+  label: 'ps4',
+  price: 44,
+  quantity: 1,
+  imgUrl:
+    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.co.uk%2FElectronic-Arts-221545-FIFA-PS4%2Fdp%2FB07DLXBGN8&psig=AOvVaw2-or8qXd_e2E66fpm3NnBK&ust=1610531300354000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKjlpuKOlu4CFQAAAAAdAAAAABAN',
+};
+
+const onRemove = jest.fn();
+const onIncrease = jest.fn();
+const onDecrease = jest.fn();
+
+let item;
+
+describe('render cart item', () => {
+  beforeEach(() => {
+    item = render(
+      <CartItem
+        props={product}
+        onRemove={onRemove}
+        onDecrease={onDecrease}
+        onIncrease={onIncrease}
+      />
+    );
+  });
+
+  it('should call onRemove when click remove', () => {
+    const remove = item.getByText('Remove');
+    fireEvent.click(remove);
+    expect(onRemove).toBeCalledTimes(1);
+  });
+
+  it('should call onDecrease when click remove', () => {
+    fireEvent.click(item.getAllByRole('button')[0]);
+    expect(onDecrease).toBeCalledTimes(1);
+  });
+
+  it('should call onIncrease when click remove', () => {
+    fireEvent.click(item.getAllByRole('button')[1]);
+    expect(onIncrease).toBeCalledTimes(1);
+  });
+});
